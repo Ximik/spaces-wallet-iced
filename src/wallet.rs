3fd6738a22e60234a0bf97429f29052cec4d3440
Alex Tsokurov <me@ximik.net>
2024-10-25 02:34:16 +0200

    wallet rename

---
+++ src/wallet.rs
@@ -0,0 +1,65 @@
+use iced::widget::qr_code::Data as QrCode;
+use spaced::wallets;
+use wallet::bitcoin::Amount;
+
+pub use wallets::AddressKind;
+pub use wallets::JointBalance;
+
+#[derive(Debug)]
+pub struct Address {
+    pub text: String,
+    pub qr_code: QrCode,
+}
+
+#[derive(Debug)]
+pub struct Wallet {
+    name: String,
+    coin_address: Option<Address>,
+    space_address: Option<Address>,
+    confirmed_balance: Amount,
+    unconfirmed_balance: Amount,
+}
+
+impl Wallet {
+    pub fn new(name: String) -> Self {
+        Self {
+            name,
+            coin_address: None,
+            space_address: None,
+            confirmed_balance: Amount::default(),
+            unconfirmed_balance: Amount::default(),
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn get_address(&self, address_kind: AddressKind) -> Option<&Address> {
+        match address_kind {
+            AddressKind::Coin => self.coin_address.as_ref(),
+            AddressKind::Space => self.space_address.as_ref(),
+        }
+    }
+
+    pub fn set_address(&mut self, address_kind: AddressKind, address: String) {
+        let qr_code = QrCode::new(&address).unwrap();
+        let address = Some(Address {
+            text: address,
+            qr_code,
+        });
+        match address_kind {
+            AddressKind::Coin => self.coin_address = address,
+            AddressKind::Space => self.space_address = address,
+        }
+    }
+
+    pub fn set_balance(&mut self, balance: JointBalance) {
+        self.confirmed_balance = balance.confirmed.total;
+        self.unconfirmed_balance = balance.confirmed.total;
+    }
+
+    pub fn get_balance(&self) -> String {
+        self.confirmed_balance.to_string_with_denomination(wallet::bitcoin::Denomination::Satoshi)
+    }
+}
